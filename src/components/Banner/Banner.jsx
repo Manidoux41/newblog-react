d08529b05d4d0bf28b1e8519c3ec6c8bdf13c58d
Manidoux41 <lherissondu41@gmail.com>
2023-02-26 12:43:45 +0100

    link to page logements

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ title,bannerImg }) {
       className="h-28 rounded-2xl bg-gray-200 overflow-hidden flex justify-center items-center brightness-90 bg-no-repeat bg-center bg-cover sm:h-56"
       style={{ backgroundImage: `url(${bannerImageUrl})` }}
     >
-      <h2 className="absolute text-white text-4xl">{title}</h2>
+      <h2 className="absolute text-white text-5xl">{title}</h2>
     </div>
   );
 }
