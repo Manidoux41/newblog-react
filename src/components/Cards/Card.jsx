0a4d3b4413c999cbd6e7209fdd6ebc451e706cd7
Manidoux41 <lherissondu41@gmail.com>
2023-02-26 10:09:11 +0100

    correction background image in style property

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 function Card({ cover, title }) {
+  const imageURL = cover;
+
   return (
     <Link
       id="card"
-      className="w-[340px] h-[340px] mx-auto justify-self-center self-center relative rounded-md overflow-hidden"
+      className="w-[340px] h-[340px] mx-auto justify-self-center self-center relative rounded-md overflow-hidden bg-center bg-cover drop-shadow-sm hover:drop-shadow-2xl hover:brightness-75"
+      style={{ backgroundImage: `url(${imageURL})` }}
     >
-      <img src={cover} alt="cover" className="w-[100%] h-[100%] object-cover" />
-      <h3 className="text-white text-lg absolute left-2 bottom-2 z-10">
+      <h3 className="text-white drop-shadow-2xl text-lg absolute left-2 bottom-2 z-10">
         {title}
       </h3>
     </Link>
